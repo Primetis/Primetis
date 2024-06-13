64e64404dd1f277b40eeb1bfd74d30d105f7b2e4
Thanh Phong <pttp15122002@gmail.com>
2024-06-13 10:40:27 +1000

    Style: remove border

--- src/components/Header/authentication.tsx
+++ src/components/Header/authentication.tsx
@@ -6,7 +6,7 @@ export default function Authentication() {
       <Link href={"/auth/signin"} className="hidden lg:block">
         Log In
       </Link>
-      <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+      <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 border-none">
         Sign Up
       </button>
     </div>
