71f56b0fb7c8275ca716373c0bde1aed38bdbda3
Thanh Phong <pttp15122002@gmail.com>
2024-06-13 14:04:36 +1000

    Style: increase line height and total height

--- src/components/Home/Hero/content.tsx
+++ src/components/Home/Hero/content.tsx
@@ -25,7 +25,7 @@ export default function Content() {
   return (
     <div className="flex flex-col items-center lg:items-start gap-y-6 text-center lg:text-left lg:gap-y-10 lg:w-1/2 self-center">
       <TypewriterEffect
-        className="lg:text-left h-[63px] md:h-fit lg:h-[84px] lg:text-5xl text-3xl text-center"
+        className="lg:text-left h-[78.75px] md:h-fit !leading-normal lg:h-[126px] lg:text-5xl text-3xl text-center"
         words={words}
       />
       <p className="lg:text-xl">
