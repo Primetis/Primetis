bf5ac9f5f31c4735c67b9d06c56771408cf97b58
Thanh Phong <pttp15122002@gmail.com>
2024-06-13 15:32:14 +1000

    Feat: update content

--- src/components/Home/Description/benifits.tsx
+++ src/components/Home/Description/benifits.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import BenifitCard from "./benifitCard";
+import { FileStack, MessageCircleQuestion, School } from "lucide-react";
 
 export type Benifit = {
   icon: ReactNode;
@@ -10,23 +11,25 @@ export type Benifit = {
 export default function Benifits() {
   const benifits = [
     {
-      icon: <></>,
+      icon: <FileStack className="text-primary lg:size-24 size-10" />,
       heading: "Beautiful and Various Templates",
-      desc: "",
+      desc: "Explore Primetis and discover a diverse selection of beautifully crafted templates designed to suit every style and purpose. Whether you're showcasing a portfolio, launching a startup, or establishing an online presence, our templates offer stunning aesthetics and easy customization to help you stand out effortlessly.",
     },
     {
-      icon: <></>,
+      icon: <School className="text-primary lg:size-24 size-10" />,
       heading: "Suitable For Many Professions",
-      desc: "",
+      desc: "Primetis offers versatile templates tailored for professionals across diverse industries. From corporate to creative fields, our templates reflect modern trends and best practices, empowering you to showcase your expertise effectively and professionally.",
     },
     {
-      icon: <></>,
+      icon: (
+        <MessageCircleQuestion className="text-primary lg:size-24 size-10" />
+      ),
       heading: "Strong Support",
-      desc: "",
+      desc: "At Primetis, we prioritize your success with reliable support at every step. Our dedicated team ensures prompt assistance, whether you need guidance on template customization or technical help, ensuring a smooth experience as you build and manage your online presence.",
     },
   ];
   return (
-    <div className="flex lg:flex-row jusitfy-between gap-x-4">
+    <div className="flex flex-col md:w-3/5 lg:w-auto md:mx-auto lg:mx-0 lg:flex-row lg:jusitfy-between gap-x-14 gap-y-10">
       {benifits.map((benifit) => {
         return <BenifitCard benifit={benifit} />;
       })}
