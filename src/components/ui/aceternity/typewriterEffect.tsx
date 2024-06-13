ae3777d8bbc29efe9ca8f6f82af05540c7f5503f
Thanh Phong <pttp15122002@gmail.com>
2024-06-13 13:57:26 +1000

    Style: update cursor height on small screens

--- src/components/ui/aceternity/typewriterEffect.tsx
+++ src/components/ui/aceternity/typewriterEffect.tsx
@@ -88,7 +88,7 @@ export const TypewriterEffect = ({
           repeatType: "reverse",
         }}
         className={cn(
-          "inline-block rounded-sm w-[4px] h-4 md:h-6 lg:h-10 bg-primary",
+          "inline-block rounded-sm w-[4px] h-6 md:h-8 lg:h-10 bg-primary",
           cursorClassName
         )}
       ></motion.span>
